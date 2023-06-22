5e833fc826c48b7610506c57ff9d04a00ed8668b
Amjad mehmood <73718759+amjad-awan@users.noreply.github.com>
2023-06-22 13:34:57 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import colors from "colors"
 const connectDB= async()=>{
 try{
-const conn= await mongoose.connect("mongodb+srv://amjadmalikf53:sb9om1oUz5lNUjeq@cluster0.fl1mjop.mongodb.net/ecommerce);
+const conn= await mongoose.connect("mongodb+srv://amjadmalikf53:sb9om1oUz5lNUjeq@cluster0.fl1mjop.mongodb.net/ecommerce");
 
 console.log(`data base is connected to host ${conn.connection.host}`.bgMagenta.white)
 }catch(error){
